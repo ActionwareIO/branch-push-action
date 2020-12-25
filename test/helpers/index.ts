10be3fe1bb076c83aa44571c1453181c91974dda
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-25 16:02:48 +0100

    Fix tests on CI

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -23,6 +23,7 @@ export async function makeWorkspace(
  */
 export function getFilteringExec(realExec: Exec): Exec {
   const isCI = !!process.env.CI
+
   return (...args: any[]) => {
     if (args[0].startsWith('git push')) {
       return
